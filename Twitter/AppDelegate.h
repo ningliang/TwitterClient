501edd7dad698db3758444433fbc002bc4ce4e62
Ning Liang <ning.q.liang@gmail.com>
2014-03-31 00:00:38 -0700

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Ning Liang on 3/31/14.
+//  Copyright (c) 2014 Ning Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
